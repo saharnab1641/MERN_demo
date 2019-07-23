ec7c26ed6ff0d0c05b4ede3aa91bb11b0de84963
Arnab Saha <saharnab1641@gmail.com>
2019-07-23 23:52:46 +0530

    Establishing db connection and adding env vars

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.get("/", (req, res) => res.send("Hello"));
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
