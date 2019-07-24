7271ca86381389a3d56353b90c2f04d35a5108b3
Arnab Saha <saharnab1641@gmail.com>
2019-07-24 13:57:10 +0530

    Added user model

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+const User = require("../models/User");
+
+router.post(
+  "/",
+  [
+    check("name", "Please add a name ")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("Passed");
+  }
+);
 
 module.exports = router;
